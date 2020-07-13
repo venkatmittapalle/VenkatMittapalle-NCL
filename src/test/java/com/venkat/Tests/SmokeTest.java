cf67d1d788670d469cf2b19b63c3045c49a8480d
venkatmittapalle <venkatmittapalle@gmail.com>
2020-07-13 13:35:58 -0400

    Add new smoke test for shore excursions price filter

--- src/test/java/com/venkat/Tests/SmokeTest.java
+++ src/test/java/com/venkat/Tests/SmokeTest.java
@@ -4,6 +4,7 @@ import com.venkat.Base.BaseClass;
 import com.venkat.Pages.LoginPage;
 import com.venkat.Pages.HomePage;
 import com.venkat.Pages.PortsPage;
+import com.venkat.Pages.ShoreExcursionsPage;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,12 +22,14 @@ public class SmokeTest extends BaseClass {
     private HomePage homePage;
     private LoginPage loginPage;
     private PortsPage portsPage;
+    private ShoreExcursionsPage shoreExcursionsPage;
 
     @BeforeEach
     public void initEach() {
         homePage = new HomePage(driver);
         loginPage = new LoginPage(driver);
         portsPage = new PortsPage(driver);
+        shoreExcursionsPage = new ShoreExcursionsPage(driver);
     }
 
     @Test
@@ -39,17 +42,49 @@ public class SmokeTest extends BaseClass {
         waitInSeconds(5);
 
         // AND
-        homePage.hoverToExploreNavigation().clickOnPorts();
-        waitInSeconds(5);
+        homePage.hoverToExploreNavigation();
+        waitInSeconds(2);
+        homePage.clickOnPorts();
+        waitInSeconds(3);
 
         // WHEN
         portsPage.searchPort(PORT).clickOnResult();
-        waitInSeconds(5);
+        waitInSeconds(3);
 
         // THEN
         Assert.assertEquals(portsPage.getSearchBarValue(), "Honolulu, Oahu");
 
         // AND
         Assert.assertEquals(portsPage.getPortInformation(), "PORT OF DEPARTURE");
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Search Shore Excursions based on price range")
+    public void smokeTestShoreExcursions() {
+        // GIVEN
+        homePage.clickOnLoginPageLink();
+        loginPage.enterUsername(USERNAME).enterPassword(PASSWORD).clickOnLoginButton();
+        waitInSeconds(5);
+
+        // AND
+        homePage.hoverToExploreNavigation();
+        waitInSeconds(2);
+        homePage.clickOnShoreExcursions();
+        waitInSeconds(3);
+
+        // WHEN
+        shoreExcursionsPage.clickOnFindExcursions();
+
+        // THEN
+        shoreExcursionsPage.setPriceRange();
+        waitInSeconds(3);
+
+        // AND
+        Assert.assertEquals(shoreExcursionsPage.getFilteredPriceRange(), "$0-$30");
+
+        // AND
+        Assert.assertEquals(shoreExcursionsPage.verifyAdultPriceRange(), true);
+        Assert.assertEquals(shoreExcursionsPage.verifyChildPriceRange(), true);
     }
 }
